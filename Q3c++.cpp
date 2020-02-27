77d0e5321990ec54a9b24feb92f7efb1b588f81a
usama-muf <usama.muf@gmail.com>
2020-02-27 23:36:51 +0530

    some updates in Q3c++.cpp file

--- Q3c++.cpp
+++ Q3c++.cpp
@@ -1,3 +1,35 @@
 #include<iostream>
 using namespace std;
 
+class acc_detail
+{	public:
+	char name[30];
+	long int acc_no;
+	char acc_type[8];
+	double balance;
+	
+};
+int assign(char n,long int accno, char type, double bal)
+{
+return n;
+return accno;
+return type;
+return bal;
+}
+int deposit(int amt)
+{
+	
+}
+int main()
+{	int n, accno,type,bal;
+	cout<<"Enter Name of Employee :";
+gets(n);
+	cout<<"Enter Account Number of Employee :";
+	cin>>accno;
+	cout<<"Enter Account type of Employee :";
+gets(type);
+	cout<<"Enter balance of Employee :";
+	cin>>bal;
+	cout<<"Initial Values: "<<assign(n, accno,type,bal);
+	return 1;
+}
