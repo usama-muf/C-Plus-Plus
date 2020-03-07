5b18b5084ac624821ea20f83323fcb7e23a215e7
usama-muf <usama.muf@gmail.com>
2020-03-07 11:49:04 +0530

    Updated Q5

--- Q5.cpp
+++ Q5.cpp
@@ -6,26 +6,26 @@ class Time
 	int hour[2],min[2],sec[2];
 	int add;
 	public:
-		int input();
-		int add_time();
-		int display_time();
+		void input();
+		int sum();
+		void display();
 }; 
-int Time::input() 
+void Time::input() 
 {	int i;
 	for(i=1;i<=2;i++)	
-	{		cout<<"Enter hour , minutes, and seconds: \n";
+	{		cout<<"Enter hour , minutes and seconds: \n";
 
 		cin>>hour[i]>>min[i]>>sec[i];
 		cout<<".........................................\n";
 	}
 }
 
-int Time::add_time()
+int Time::sum()
 {
 int tot_hr,tot_min,tot_sec;
 tot_hr=hour[1]+hour[2];
 tot_min=min[1]+min[2];
-if(tot_min>60&&tot_min<120)
+if(tot_min>=60&&tot_min<120)
 {tot_hr++;
 tot_min-=60;
 }
@@ -35,7 +35,7 @@ tot_hr+=2;
 tot_min-=120;
 }
 tot_sec=sec[1]+sec[2];
-if(tot_sec>60&&tot_sec<120)
+if(tot_sec>=60&&tot_sec<120)
 {tot_min++;
 tot_sec-=60;}
 else
@@ -48,7 +48,7 @@ return tot_min;
 return tot_sec;
 	
 }
-int Time::display_time()
+void Time::display()
 {int tot_hr,tot_min,tot_sec;
 
 	cout<<"\ntotal time is \n"<<tot_hr<<" hr"<<" :"<<tot_min<<" min"<<" :"<<tot_sec<<" sec"<<endl;
@@ -60,8 +60,8 @@ int main()
 	
 	Time t1;
 	t1.input();
-	t1.add_time();
-	t1.display_time();
+	t1.sum();
+	t1.display();
 	return 0;
 	
 }
